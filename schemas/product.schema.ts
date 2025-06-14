54aa8de89f025286c8e230948866a2478a5d2089
Muhammad Farhan <farhan_75230@hotmail.com>
2025-06-14 14:18:04 +0200

    DTOs generated!

---
+++ schemas/product.schema.ts
@@ -0,0 +1,64 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type ProductDocument = HydratedDocument<Product>;
+
+@Schema()
+export class Product {
+  @Prop()
+  name: string;
+
+  @Prop()
+  description: string;
+
+  @Prop()
+  price: number;
+
+  @Prop()
+  quantity: number;
+
+  @Prop()
+  createdAt: Date;
+}
+
+export const ProductSchema = SchemaFactory.createForClass(Product);
+
+// import mongoose from 'mongoose';
+
+// export const productSchema = new mongoose.Schema(
+//   {
+//     name: {
+//       type: String,
+//       required: true,
+//     },
+//     description: {
+//       type: String,
+//       required: true,
+//     },
+//     price: {
+//       type: Number,
+//       required: true,
+//     },
+//     quantity: {
+//       type: Number,
+//       required: true,
+//     },
+//     createdAt: {
+//       type: Date,
+//       required: true,
+//     },
+//   },
+//   {
+//     timestamps: true,
+//   },
+// );
+
+// export interface Product extends mongoose.Document {
+//   name: string;
+//   description: string;
+//   price: number;
+//   quantity: number;
+//   createdAt: Date;
+// }
+
+//https://dev.to/manthanank/building-a-crud-application-with-nestjs-and-mongodb-52o2
