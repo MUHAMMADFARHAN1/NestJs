2a42dcbf4e09dfaa5215e95b116be82870f0ac32
Muhammad Farhan <farhan_75230@hotmail.com>
2025-06-14 12:29:39 +0200

    Basic routes generated!

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -9,10 +9,28 @@ import {
   Delete,
 } from '@nestjs/common';
 
+type ProductParams = { id: string };
+
 @Controller('products')
 export class ProductsController {
   @Get()
   findAll() {
     return 'There are products in the System';
+  }
+
+  @Post()
+  postOne() {
+    return 'There are products in the System';
+  }
+
+  @Get(':id')
+  findOne(@Param() params: ProductParams) {
+    let { id } = params;
+    return `Fetching the product ${id}`;
+  }
+
+  @Delete()
+  DeleteOne() {
+    return 'There are deletes in the System';
   }
 }
