b2a448b587b778483532ea428b4d2cf0d0488af8
Muhammad Farhan <farhan_75230@hotmail.com>
2025-06-14 14:45:44 +0200

    updated!

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -9,33 +9,49 @@ import {
   Delete,
 } from '@nestjs/common';
 
+import { Product, ProductSchema } from './../../schemas/product.schema';
+import { CreateProductDto } from './../../dto/create-product.dto';
+import { UpdateProductDto } from './../../dto/update-product.dto';
+import { ProductsService } from './products.service';
+
 type ProductParams = { id: string };
 
 @Controller('products')
 export class ProductsController {
+  constructor(private readonly productsService: ProductsService) {}
+
+  // @Post()
+  // async create(@Body() createCatDto: CreateProductDto) {
+  //   return this.catsService.create(createCatDto);
+  // }
   @Get()
-  findAll() {
-    return 'There are products in the System';
+  async findAll(): Promise<Product[]> {
+    return this.productsService.findAll();
   }
 
-  @Post()
-  postOne() {
-    return 'There are products in the System';
-  }
+  // @Get()
+  // findAll() {
+  //   return 'There are products in the System';
+  // }
 
-  @Put()
-  putOne() {
-    return 'There are puts in the System';
-  }
+  // @Post()
+  // postOne() {
+  //   return 'There are products in the System';
+  // }
 
-  @Get(':id')
-  findOne(@Param() params: ProductParams) {
-    let { id } = params;
-    return `Fetching the product ${id}`;
-  }
+  // @Put()
+  // putOne() {
+  //   return 'There are puts in the System';
+  // }
 
-  @Delete()
-  DeleteOne() {
-    return 'There are deletes in the System';
-  }
+  // @Get(':id')
+  // findOne(@Param() params: ProductParams) {
+  //   let { id } = params;
+  //   return `Fetching the product ${id}`;
+  // }
+
+  // @Delete()
+  // DeleteOne() {
+  //   return 'There are deletes in the System';
+  // }
 }
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -11,32 +11,32 @@ export class ProductsService {
     @InjectModel(Product.name) private readonly ProductModel: Model<Product>,
   ) {}
 
-  async create(createProductDto: CreateProductDto): Promise<Product> {
-    const createdProduct = await this.ProductModel.create(createProductDto);
-    return createdProduct;
-  }
+  //   async create(createProductDto: CreateProductDto): Promise<Product> {
+  //     const createdProduct = await this.ProductModel.create(createProductDto);
+  //     return createdProduct;
+  //   }
 
   async findAll(): Promise<Product[]> {
     return this.ProductModel.find().exec();
   }
 
-  async findOne(id: string): Promise<Product> {
-    return this.ProductModel.findOne({ _id: id }).exec();
-  }
+  //   async findOne(id: string): Promise<Product> {
+  //     return this.ProductModel.findOne({ _id: id }).exec();
+  //   }
 
-  async update(
-    id: string,
-    updateProductDto: UpdateProductDto,
-  ): Promise<Product> {
-    return this.ProductModel.findByIdAndUpdate({ _id: id }, updateProductDto, {
-      new: true,
-    }).exec();
-  }
+  //   async update(
+  //     id: string,
+  //     updateProductDto: UpdateProductDto,
+  //   ): Promise<Product> {
+  //     return this.ProductModel.findByIdAndUpdate({ _id: id }, updateProductDto, {
+  //       new: true,
+  //     }).exec();
+  //   }
 
-  async delete(id: string): Promise<Product> {
-    const deletedProduct = await this.ProductModel.findByIdAndDelete({
-      _id: id,
-    }).exec();
-    return deletedProduct;
-  }
+  //   async delete(id: string): Promise<Product> {
+  //     const deletedProduct = await this.ProductModel.findByIdAndDelete({
+  //       _id: id,
+  //     }).exec();
+  //     return deletedProduct;
+  //   }
 }
